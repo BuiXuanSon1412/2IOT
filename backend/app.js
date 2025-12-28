b4d3f59d69b95bd10305abc66dd770644241e15f
NguyenChanHung1 <nchhdhn@gmail.com>
2025-12-28 10:49:15 +0700

    add: sensor and device timestamp data

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/board", boardRoutes);
 
 async function bootstrap() {
-  console.log(new Date(1735736400000).toLocaleString("vi-VN"));
+  console.log(new Date(1766379600000).toLocaleString("vi-VN"));
 
   await connectDB(mongoDbUri);
 
